a2d28acebbcdbca3621d919b82b70d77ec6b4eb3
Allen Bierbaum <abierbaum@gmail.com>
2011-12-23 08:45:05 -0600

    Add simple "app"

---
+++ src/app.py
@@ -0,0 +1,11 @@
+import utils
+
+
+print "Starting up the app"
+
+
+utils.doSomething(10)
+
+
+utils.doSomething2(15)
+
---
+++ src/utils.py
@@ -0,0 +1,10 @@
+
+
+def doSomething(value):
+  print "Did something with: ", value
+  
+
+def doSomething2(value):
+  print "Did something2: ", value
+
+
