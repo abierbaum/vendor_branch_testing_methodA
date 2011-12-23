695cada056db91c7c7a5401c894eb3a5df62b7d1
Allen Bierbaum <abierbaum@gmail.com>
2011-12-23 08:51:44 -0600

    Add a new method that we want to use. (cherry picked from commit 058b0b5bdf1c3fdb6549510984f0888499097860)

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,9 @@
 
 
-def doSomething(value):
+def doSomething(value, other = None):
   print "Did something with: ", value
+  if other is not None:
+    print "doing more: ", other
   
 
 def doSomething2(value):
