0b7038fb7a6fe7aab6faeeed553af51b2409639d
Allen Bierbaum <abierbaum@gmail.com>
2011-12-23 09:58:00 -0600

    Add new method for refactor 2. (cherry picked from commit 6bf9ca4c1b9bf6892ba0ad92e288c8655fb829b2)
    Conflicts:
    
    	src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,9 @@ def doSomething2(value):
   print "Did something2: ", value
 
 
+def commonMethod(value):
+  print "common support: ", value
+
+def refactor2Method(value):
+  print "value: ", value
+
